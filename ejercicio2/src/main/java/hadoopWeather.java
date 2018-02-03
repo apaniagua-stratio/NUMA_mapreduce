853b572a0e1c98f319404fe141204a4b4c60a0ca
apaniagua-stratio <apaniagua@stratio.com>
2018-02-03 16:56:01 +0100

    exercise 2

--- ejercicio2/src/main/java/hadoopWeather.java
+++ ejercicio2/src/main/java/hadoopWeather.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -10,11 +12,18 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
+
 public class hadoopWeather {
 
     public static class MaxTemperatureMapper
             extends Mapper<LongWritable, Text, Text, IntWritable> {
         private static final int MISSING = 99999;
+
+        enum INCORRECTAS {
+            TOO_LOW,
+            TOO_HIGH
+        }
 
         @Override
         public void map(LongWritable key, Text value, Context context)
@@ -22,24 +31,25 @@ public class hadoopWeather {
 
             String line = value.toString();
             String year = line.substring(21, 25);
-            int airTemperature;
-            airTemperature = Integer.parseInt(line.substring(46, 52));
-
+            int airTemperature = Integer.parseInt(line.substring(46, 52));
             String quality = line.substring(67, 70);
 
-            if (airTemperature != MISSING && quality.matches("[1]")) {
+            if (airTemperature != MISSING) {
                 context.write(new Text(year), new IntWritable(airTemperature));
+            }
+            else
+            {
+                //context.getCounter(INCORRECTAS.TOO_LOW).increment(1);
+                context.setStatus("Detected possibly corrupt record: see logs.");
             }
         }
     }
 
-    public class MaxTemperatureReducer
+    public static class MaxTemperatureReducer
             extends Reducer<Text, IntWritable, Text, IntWritable> {
-
         @Override
         public void reduce(Text key, Iterable<IntWritable> values, Context context)
                 throws IOException, InterruptedException {
-
             int maxValue = Integer.MIN_VALUE;
             for (IntWritable value : values) {
                 maxValue = Math.max(maxValue, value.get());
@@ -48,24 +58,28 @@ public class hadoopWeather {
         }
     }
 
+
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.err.println("Usage : MaxTemperature <input path> <output path>");
             System.exit(-1);
         }
 
-        Job job = new Job();
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Max temperature");
+        //definimos la clase q contiene nuestro mapper y reducer
         job.setJarByClass(hadoopWeather.class);
-        job.setJobName("Max temperature");
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
+        //definir las clases que haran mapeo y reduce
         job.setMapperClass(MaxTemperatureMapper.class);
         job.setReducerClass(MaxTemperatureReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-
+        //definir paths de hdfs como entrada y salida del job, eran parametrizados
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        //lanzar trabajo asincronamente
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
     }
 
 }
--- ejercicio2/src/test/java/testHadoop.java
+++ ejercicio2/src/test/java/testHadoop.java
@@ -6,7 +6,8 @@ public class testHadoop {
 
     public static void main(String[] args) throws Exception {
 
-        Text txt = new Text("(106, 0043012650999992014032418004N9+0500001N9+00035+99999999999N9+[1])");
+        //Text txt = new Text("(318, 0043012650999992006032418004N9+0500001N9+00031+99999999999N9+[1])");
+        Text txt = new Text("(318, 0043012650999992016032418004N9+0500001N9+00038+99999999999N9+[1])");
 
         String line = txt.toString();
         String year = line.substring(21, 25);
@@ -17,6 +18,17 @@ public class testHadoop {
         System.out.println(year);
         System.out.println(airTemperature);
         System.out.println(quality);
+
+                    /*
+            if (airTemperature > 100) {
+                //context.getCounter(INCORRECTAS.TOO_LOW).increment(1);
+                context.setStatus("Detected possibly corrupt record: see logs.");
+            }
+            else {
+                context.write(new Text(year), new IntWritable(airTemperature));
+            }
+            ..&& quality.matches("[1]")
+            */
 
 
 
